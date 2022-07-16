c47568dd305282b23de1ae21bb53c4743a1ffd0c
ramisha-anwer <ramishaaa27@gmail.com>
2022-07-17 02:01:07 +0500

    API setup

--- src/api/index.js
+++ src/api/index.js
@@ -8,8 +8,7 @@ const options = {
       tr_latitude: '33.564072359777626',
       bl_longitude: '73.11319580761362',
       tr_longitude: '73.15619010006544',
-      
-    },
+      },
     headers: {
       'X-RapidAPI-Key': '3d9d0f548emsh2270349972d1b20p19f279jsn4977724dc2be',
       'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
